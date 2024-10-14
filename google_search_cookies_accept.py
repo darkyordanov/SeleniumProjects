4e0277fda5a6814345b674ae3949efa5c830dbf7
Yordan Yordanov (S. T. G.) <y.yordanov.stg@gmail.com>
2024-10-14 18:28:43 +0300

    Add function to print dashes.

--- google_search_cookies_accept.py
+++ google_search_cookies_accept.py
@@ -18,7 +18,9 @@ wait = WebDriverWait(driver, 10)
 
 time.sleep(2)
 
-COUNTER = 30
+def dash_print_func():
+    COUNTER = 30
+    print(COUNTER * '-')
 
 # Check if the cookie consent pop-up appears and accept it
 try:
@@ -26,10 +28,10 @@ try:
     accept_cookies_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "QS5gu") and contains(text(),"Accept all")]')))
     accept_cookies_button.click()
     print("Cookies accepted.")
-    print(COUNTER * '-')
+    dash_print_func()
 except Exception as e:
     print(f"No cookie consent pop-up found or issue with locating it: {e}")
-    print(COUNTER * '-')
+    dash_print_func()
 
 # Wait for the search input field (textarea) to be clickable
 # try:
@@ -44,19 +46,20 @@ time.sleep(2)
 try:
     input_element = driver.find_element(By.CLASS_NAME, 'gLFyf')
     print(f'Sucess click on the search bar')
-    print(COUNTER * '-')
+    dash_print_func()
     time.sleep(1)
     
     input_search_element = input('What you want to search: ')
     # input_search_element = 'bitcoin current price'
-    print(COUNTER * '-')
+    dash_print_func()
     
     input_element.send_keys(input_search_element, Keys.ENTER)
     print(f'Searching for: {input_search_element[:26]}')
-    print(COUNTER * '-')
+    dash_print_func()
+    
 except Exception as e:
     print(f"Issue with locating the search bar: {e}")
-    print(COUNTER * '-')
+    dash_print_func()
 
 time.sleep(2)
 
@@ -65,10 +68,11 @@ try:
     first_result = wait.until(EC.element_to_be_clickable((By.XPATH, '(//h3)[1]')))  # This locates the first <h3> tag in search results
     first_result.click()  # Click on the first search result
     print("Clicked on the first search result.")
-    print(COUNTER * '-')
+    dash_print_func()
+    
 except Exception as e:
     print(f"Issue with locating the first search result: {e}")
-    print(COUNTER * '-')
+    dash_print_func()
 
 # Wait for some time to view the results
 time.sleep(60)
